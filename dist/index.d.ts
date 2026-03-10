4f917a901f1a4061ab174fc30cbf1d84fe7f2485
Your GitHub Username <Priyank.verma@iphtechnologies.com>
2026-03-10 15:24:58 +0530

    fixed package json

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as VizlyChart } from '../node_modules/components/VizlyChart';
-export type { VizlyProps, VizlyRef } from '../node_modules/components/VizlyChart';
-//# sourceMappingURL=index.d.ts.map
