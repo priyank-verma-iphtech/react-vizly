855ae29a2ac2cb973aae48c21021feec9d7e1ff8
Your GitHub Username <Priyank.verma@iphtechnologies.com>
2026-03-11 11:21:09 +0530

    fixed the issues

--- dist/react-vizly.js
+++ dist/react-vizly.js
@@ -1,5 +1,5 @@
-import { jsxs as C, jsx as d } from "react/jsx-runtime";
-import f, { forwardRef as B, useRef as h, useState as E, useMemo as D, useCallback as S, useImperativeHandle as R, useEffect as O } from "react";
+import { jsxs as O, jsx as d } from "react/jsx-runtime";
+import f, { forwardRef as D, useRef as g, useState as E, useMemo as R, useCallback as S, useImperativeHandle as A, useEffect as P } from "react";
 import z from "apexcharts";
 var I = {
   color: void 0,
@@ -7,11 +7,11 @@ var I = {
   className: void 0,
   style: void 0,
   attr: void 0
-}, P = f.createContext && /* @__PURE__ */ f.createContext(I), M = ["attr", "size", "title"];
-function _(e, t) {
+}, L = f.createContext && /* @__PURE__ */ f.createContext(I), _ = ["attr", "size", "title"];
+function M(e, t) {
   if (e == null)
     return {};
-  var r, n, s = A(e, t);
+  var r, n, s = T(e, t);
   if (Object.getOwnPropertySymbols) {
     var c = Object.getOwnPropertySymbols(e);
     for (n = 0; n < c.length; n++)
@@ -19,7 +19,7 @@ function _(e, t) {
   }
   return s;
 }
-function A(e, t) {
+function T(e, t) {
   if (e == null)
     return {};
   var r = {};
@@ -31,17 +31,17 @@ function A(e, t) {
     }
   return r;
 }
-function y() {
-  return y = Object.assign ? Object.assign.bind() : function(e) {
+function h() {
+  return h = Object.assign ? Object.assign.bind() : function(e) {
     for (var t = 1; t < arguments.length; t++) {
       var r = arguments[t];
       for (var n in r)
         ({}).hasOwnProperty.call(r, n) && (e[n] = r[n]);
     }
     return e;
-  }, y.apply(null, arguments);
+  }, h.apply(null, arguments);
 }
-function L(e, t) {
+function N(e, t) {
   var r = Object.keys(e);
   if (Object.getOwnPropertySymbols) {
     var n = Object.getOwnPropertySymbols(e);
@@ -51,25 +51,25 @@ function L(e, t) {
   }
   return r;
 }
-function x(e) {
+function p(e) {
   for (var t = 1; t < arguments.length; t++) {
     var r = arguments[t] != null ? arguments[t] : {};
-    t % 2 ? L(Object(r), !0).forEach(function(n) {
-      T(e, n, r[n]);
-    }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(r)) : L(Object(r)).forEach(function(n) {
+    t % 2 ? N(Object(r), !0).forEach(function(n) {
+      V(e, n, r[n]);
+    }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(r)) : N(Object(r)).forEach(function(n) {
       Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(r, n));
     });
   }
   return e;
 }
-function T(e, t, r) {
-  return (t = V(t)) in e ? Object.defineProperty(e, t, { value: r, enumerable: !0, configurable: !0, writable: !0 }) : e[t] = r, e;
+function V(e, t, r) {
+  return (t = X(t)) in e ? Object.defineProperty(e, t, { value: r, enumerable: !0, configurable: !0, writable: !0 }) : e[t] = r, e;
 }
-function V(e) {
-  var t = X(e, "string");
+function X(e) {
+  var t = H(e, "string");
   return typeof t == "symbol" ? t : t + "";
 }
-function X(e, t) {
+function H(e, t) {
   if (typeof e != "object" || !e)
     return e;
   var r = e[Symbol.toPrimitive];
@@ -82,147 +82,133 @@ function X(e, t) {
   return (t === "string" ? String : Number)(e);
 }
 function k(e) {
-  return e && e.map((t, r) => /* @__PURE__ */ f.createElement(t.tag, x({
+  return e && e.map((t, r) => /* @__PURE__ */ f.createElement(t.tag, p({
     key: r
   }, t.attr), k(t.child)));
 }
-function N(e) {
-  return (t) => /* @__PURE__ */ f.createElement(H, y({
-    attr: x({}, e.attr)
+function B(e) {
+  return (t) => /* @__PURE__ */ f.createElement(W, h({
+    attr: p({}, e.attr)
   }, t), k(e.child));
 }
-function H(e) {
+function W(e) {
   var t = (r) => {
     var {
       attr: n,
       size: s,
       title: c
-    } = e, m = _(e, M), i = s || r.size || "1em", l;
-    return r.className && (l = r.className), e.className && (l = (l ? l + " " : "") + e.className), /* @__PURE__ */ f.createElement("svg", y({
+    } = e, m = M(e, _), a = s || r.size || "1em", o;
+    return r.className && (o = r.className), e.className && (o = (o ? o + " " : "") + e.className), /* @__PURE__ */ f.createElement("svg", h({
       stroke: "currentColor",
       fill: "currentColor",
       strokeWidth: "0"
     }, r.attr, n, m, {
-      className: l,
-      style: x(x({
+      className: o,
+      style: p(p({
         color: e.color || r.color
       }, r.style), e.style),
-      height: i,
-      width: i,
+      height: a,
+      width: a,
       xmlns: "http://www.w3.org/2000/svg"
     }), c && /* @__PURE__ */ f.createElement("title", null, c), e.children);
   };
-  return P !== void 0 ? /* @__PURE__ */ f.createElement(P.Consumer, null, (r) => t(r)) : t(I);
-}
-function W(e) {
-  return N({ tag: "svg", attr: { viewBox: "0 0 24 24" }, child: [{ tag: "g", attr: { id: "Maximize_1" }, child: [{ tag: "g", attr: {}, child: [{ tag: "path", attr: { d: "M3.065,16.16a.5.5,0,0,1,1,0v3.07l.01-.01,6.07-6.07a.5.5,0,0,1,.71.71c-.29.29-.58.57-.87.86-1.74,1.74-3.47,3.48-5.21,5.22h3.07a.5.5,0,0,1,0,1H3.565a.429.429,0,0,1-.34-.14c-.01-.01-.02-.01-.02-.02a.384.384,0,0,1-.13-.26C3.066,20.442,3.065,16.16,3.065,16.16Z" }, child: [] }, { tag: "path", attr: { d: "M20.935,3.56V7.84a.5.5,0,0,1-1,0V4.77l-.01.01q-3.045,3.03-6.07,6.07a.5.5,0,0,1-.71-.71c.29-.29.58-.57.86-.86,1.75-1.74,3.48-3.48,5.22-5.22h-3.07a.5.5,0,0,1,0-1h4.28a.429.429,0,0,1,.34.14c.01.01.02.01.02.02A.429.429,0,0,1,20.935,3.56Z" }, child: [] }] }] }] })(e);
+  return L !== void 0 ? /* @__PURE__ */ f.createElement(L.Consumer, null, (r) => t(r)) : t(I);
 }
 function Z(e) {
-  return N({ tag: "svg", attr: { viewBox: "0 0 15 15", fill: "none" }, child: [{ tag: "path", attr: { d: "M12.2248 2.08224C12.4189 1.95407 12.6829 1.97583 12.8537 2.14669C13.0246 2.31756 13.0463 2.58152 12.9182 2.7756L12.8537 2.85372L8.20724 7.50021L12.8537 12.1467L12.9182 12.2248C13.0463 12.4189 13.0246 12.6829 12.8537 12.8537C12.6829 13.0246 12.4189 13.0463 12.2248 12.9182L12.1467 12.8537L7.50021 8.20724L2.85372 12.8537C2.65846 13.049 2.34195 13.049 2.14669 12.8537C1.95143 12.6585 1.95143 12.342 2.14669 12.1467L6.79318 7.50021L2.14669 2.85372L2.08224 2.7756C1.95407 2.58152 1.97583 2.31756 2.14669 2.14669C2.31756 1.97583 2.58152 1.95407 2.7756 2.08224L2.85372 2.14669L7.50021 6.79318L12.1467 2.14669L12.2248 2.08224Z", fill: "currentColor" }, child: [] }] })(e);
+  return B({ tag: "svg", attr: { viewBox: "0 0 24 24" }, child: [{ tag: "g", attr: { id: "Maximize_1" }, child: [{ tag: "g", attr: {}, child: [{ tag: "path", attr: { d: "M3.065,16.16a.5.5,0,0,1,1,0v3.07l.01-.01,6.07-6.07a.5.5,0,0,1,.71.71c-.29.29-.58.57-.87.86-1.74,1.74-3.47,3.48-5.21,5.22h3.07a.5.5,0,0,1,0,1H3.565a.429.429,0,0,1-.34-.14c-.01-.01-.02-.01-.02-.02a.384.384,0,0,1-.13-.26C3.066,20.442,3.065,16.16,3.065,16.16Z" }, child: [] }, { tag: "path", attr: { d: "M20.935,3.56V7.84a.5.5,0,0,1-1,0V4.77l-.01.01q-3.045,3.03-6.07,6.07a.5.5,0,0,1-.71-.71c.29-.29.58-.57.86-.86,1.75-1.74,3.48-3.48,5.22-5.22h-3.07a.5.5,0,0,1,0-1h4.28a.429.429,0,0,1,.34.14c.01.01.02.01.02.02A.429.429,0,0,1,20.935,3.56Z" }, child: [] }] }] }] })(e);
 }
-const F = (e) => {
-  if (!e || e.length === 0)
+function F(e) {
+  return B({ tag: "svg", attr: { viewBox: "0 0 15 15", fill: "none" }, child: [{ tag: "path", attr: { d: "M12.2248 2.08224C12.4189 1.95407 12.6829 1.97583 12.8537 2.14669C13.0246 2.31756 13.0463 2.58152 12.9182 2.7756L12.8537 2.85372L8.20724 7.50021L12.8537 12.1467L12.9182 12.2248C13.0463 12.4189 13.0246 12.6829 12.8537 12.8537C12.6829 13.0246 12.4189 13.0463 12.2248 12.9182L12.1467 12.8537L7.50021 8.20724L2.85372 12.8537C2.65846 13.049 2.34195 13.049 2.14669 12.8537C1.95143 12.6585 1.95143 12.342 2.14669 12.1467L6.79318 7.50021L2.14669 2.85372L2.08224 2.7756C1.95407 2.58152 1.97583 2.31756 2.14669 2.14669C2.31756 1.97583 2.58152 1.95407 2.7756 2.08224L2.85372 2.14669L7.50021 6.79318L12.1467 2.14669L12.2248 2.08224Z", fill: "currentColor" }, child: [] }] })(e);
+}
+const K = (e) => {
+  if (!Array.isArray(e) || e.length === 0)
     return "bar";
   const t = e[0];
-  return typeof t == "number" ? "donut" : (t == null ? void 0 : t.x) !== void 0 && (t == null ? void 0 : t.y) !== void 0 ? t.x instanceof Date || !isNaN(Date.parse(t.x)) && isNaN(t.x) ? "area" : Array.isArray(t.y) ? "rangeBar" : "line" : (t == null ? void 0 : t.label) !== void 0 && (t == null ? void 0 : t.value) !== void 0 ? "donut" : "bar";
-}, te = B(
+  return typeof t == "number" ? "donut" : (t == null ? void 0 : t.x) !== void 0 && (t == null ? void 0 : t.y) !== void 0 ? t.x instanceof Date || typeof t.x == "string" && isNaN(Number(t.x)) && !isNaN(Date.parse(t.x)) ? "area" : Array.isArray(t.y) ? "rangeBar" : (t == null ? void 0 : t.z) !== void 0 ? "bubble" : "line" : (t == null ? void 0 : t.label) !== void 0 && (t == null ? void 0 : t.value) !== void 0 ? "donut" : "bar";
+}, re = D(
   ({ data: e, type: t, options: r = {}, height: n = 350 }, s) => {
-    const c = h(null), m = h(null), i = h(null), l = h(null), [v, j] = E(!1), b = D(() => t || F(e), [e, t]), w = S((o, a) => ["pie", "donut", "radialBar"].includes(a) ? o.map((u) => typeof u == "object" ? u.value : u) : [{ name: "Series 1", data: o }], []);
-    R(s, () => ({
+    const c = g(null), m = g(null), a = g(null), o = g(null), [v, j] = E(!1), y = R(() => t || K(e), [e, t]), x = S((i, l) => {
+      const u = Array.isArray(i) ? i : [];
+      return ["pie", "donut", "radialBar"].includes(l) ? u.length > 0 ? u.map((C) => typeof C == "object" ? C.value : C) : [] : [{ name: (r == null ? void 0 : r.seriesName) || "Series 1", data: u }];
+    }, [r == null ? void 0 : r.seriesName]);
+    A(s, () => ({
       zoomIn: () => {
-        if (!i.current)
+        const i = a.current;
+        if (!(i != null && i.w))
           return;
-        const o = i.current, { min: a, max: p } = o.w.globals.lastXAxis || {
-          min: o.w.globals.minX,
-          max: o.w.globals.maxX
-        }, u = (p - a) * 0.2;
-        i.current.zoomX(a + u, p - u);
+        const { min: l, max: u } = i.w.globals.lastXAxis || { min: i.w.globals.minX, max: i.w.globals.maxX }, w = (u - l) * 0.2;
+        i.zoomX(l + w, u - w);
       },
       zoomOut: () => {
-        var o;
-        return (o = i.current) == null ? void 0 : o.resetSeries();
+        var i;
+        return (i = a.current) == null ? void 0 : i.resetSeries();
       },
-      filter: (o) => {
-        var a;
-        (a = i.current) == null || a.updateSeries(w(o, b));
+      filter: (i) => {
+        var l;
+        (l = a.current) == null || l.updateSeries(x(i, y));
       },
       reset: () => {
-        var o;
-        return (o = i.current) == null ? void 0 : o.resetSeries();
-      },
-      download: (o) => {
-        const a = l.current || i.current;
-        a == null || a.exports.exportToCSV();
+        var i;
+        return (i = a.current) == null ? void 0 : i.resetSeries();
       }
     }));
-    const g = S((o, a) => {
-      var p;
+    const b = S((i) => {
+      var l;
       return {
         ...r,
         chart: {
+          toolbar: { show: !0 },
           ...r.chart,
-          type: b,
-          height: o,
-          toolbar: {
-            show: !0,
-            // This enables the built-in Download/Zoom/Pan buttons
-            tools: {
-              download: !0,
-              // This adds the 'hamburger' menu for PNG/SVG/CSV
-              selection: !0,
-              zoom: !0,
-              zoomin: !0,
-              zoomout: !0,
-              pan: !0,
-              reset: !0
-            },
-            autoSelected: "zoom"
-          },
-          animations: { enabled: !0, easing: "easeinout", speed: 800 }
+          type: y,
+          height: i
         },
-        series: w(e, b),
-        labels: r.labels || ((p = e[0]) != null && p.label ? e.map((u) => u.label) : void 0),
-        theme: { mode: "light" }
+        series: x(e, y),
+        labels: r.labels || ((l = e[0]) != null && l.label ? e.map((u) => u.label) : void 0)
       };
-    }, [e, b, r, w]);
-    return O(() => {
+    }, [e, y, r, x]);
+    return P(() => {
       if (!c.current)
         return;
-      const o = g(n, !1);
-      i.current ? i.current.updateOptions(o) : (i.current = new z(c.current, o), i.current.render());
-    }, [g, n]), O(() => {
-      if (v && m.current) {
-        const o = g("100%", !0);
-        l.current = new z(m.current, o), l.current.render();
+      const i = b(n);
+      if (a.current)
+        a.current.updateOptions(i, !1, !0);
+      else {
+        const l = new z(c.current, i);
+        a.current = l, l.render().catch(() => {
+        });
       }
       return () => {
-        var o;
-        (o = l.current) == null || o.destroy(), l.current = null;
+        a.current && (a.current.destroy(), a.current = null);
       };
-    }, [v, g]), O(() => () => {
-      var o;
-      return (o = i.current) == null ? void 0 : o.destroy();
-    }, []), /* @__PURE__ */ C("div", { style: K, children: [
-      /* @__PURE__ */ d("button", { onClick: () => j(!0), style: q, title: "Full Screen", children: /* @__PURE__ */ d(W, { size: 20 }) }),
+    }, [b, n]), P(() => {
+      if (v && m.current) {
+        const i = b("100%");
+        o.current = new z(m.current, i), o.current.render().catch(() => {
+        });
+      }
+      return () => {
+        o.current && (o.current.destroy(), o.current = null);
+      };
+    }, [v, b]), /* @__PURE__ */ O("div", { style: q, children: [
+      /* @__PURE__ */ d("button", { onClick: () => j(!0), style: G, title: "Full Screen", children: /* @__PURE__ */ d(Z, { size: 20 }) }),
       /* @__PURE__ */ d("div", { ref: c }),
-      v && /* @__PURE__ */ d("div", { style: G, children: /* @__PURE__ */ C("div", { style: J, children: [
-        /* @__PURE__ */ C("div", { style: Q, children: [
-          /* @__PURE__ */ d("h3", { style: { margin: 0, fontSize: "1.2rem" }, children: "Detailed Visualization" }),
-          /* @__PURE__ */ d("button", { onClick: () => j(!1), style: U, children: /* @__PURE__ */ d(Z, { size: 22 }) })
+      v && /* @__PURE__ */ d("div", { style: J, children: /* @__PURE__ */ O("div", { style: Q, children: [
+        /* @__PURE__ */ O("div", { style: U, children: [
+          /* @__PURE__ */ d("h3", { style: { margin: 0 }, children: "Detailed View" }),
+          /* @__PURE__ */ d("button", { onClick: () => j(!1), style: Y, children: /* @__PURE__ */ d(F, { size: 22 }) })
         ] }),
         /* @__PURE__ */ d("div", { ref: m, style: { flex: 1, width: "100%" } })
       ] }) })
     ] });
   }
-), K = {
+), q = {
   position: "relative",
   width: "100%",
   background: "#fff",
   border: "1px solid #e2e8f0",
   borderRadius: "12px",
-  padding: "15px",
-  boxShadow: "0 1px 3px rgba(0,0,0,0.1)"
-}, q = {
+  padding: "15px"
+}, G = {
   position: "absolute",
   right: "15px",
   top: "15px",
@@ -233,10 +219,8 @@ const F = (e) => {
   cursor: "pointer",
   padding: "6px",
   display: "flex",
-  alignItems: "center",
-  color: "#475569",
-  transition: "all 0.2s"
-}, G = {
+  alignItems: "center"
+}, J = {
   position: "fixed",
   top: 0,
   left: 0,
@@ -248,34 +232,32 @@ const F = (e) => {
   alignItems: "center",
   zIndex: 9999,
   backdropFilter: "blur(4px)"
-}, J = {
+}, Q = {
   width: "90%",
   height: "85%",
   backgroundColor: "#fff",
   borderRadius: "16px",
   padding: "25px",
   display: "flex",
-  flexDirection: "column",
-  boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)"
-}, Q = {
+  flexDirection: "column"
+}, U = {
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
   marginBottom: "20px",
   paddingBottom: "15px",
   borderBottom: "1px solid #f1f5f9"
-}, U = {
+}, Y = {
   background: "#f1f5f9",
   border: "none",
   borderRadius: "50%",
   width: "36px",
   height: "36px",
+  cursor: "pointer",
   display: "flex",
   alignItems: "center",
-  justifyContent: "center",
-  cursor: "pointer",
-  color: "#64748b"
+  justifyContent: "center"
 };
 export {
-  te as VizlyChart
+  re as VizlyChart
 };
--- src/components/VizlyChart.tsx
+++ src/components/VizlyChart.tsx
@@ -24,30 +24,33 @@ export interface VizlyRef {
   zoomOut: () => void;
   filter: (newData: any[]) => void;
   reset: () => void;
-  download: (type: "png" | "csv" | "svg") => void;
 }
 
-// --- Optimized Inference Engine ---
+// --- Smart Inference Engine ---
 export const inferChartType = (data: any[]): string => {
-  if (!data || data.length === 0) return 'bar';
+  if (!Array.isArray(data) || data.length === 0) return 'bar';
+  
   const first = data[0];
 
-  // Simple number array -> Donut
+  // Simple number array -> Donut/Pie
   if (typeof first === 'number') return 'donut';
 
   // Coordinate data (x, y)
   if (first?.x !== undefined && first?.y !== undefined) {
-    // Check if x is a date-like string or object
-    const isDate = first.x instanceof Date || (!isNaN(Date.parse(first.x)) && isNaN(first.x));
+    // Check if x is a date string or object
+    const isDate = first.x instanceof Date || 
+                  (typeof first.x === 'string' && isNaN(Number(first.x)) && !isNaN(Date.parse(first.x)));
+    
     if (isDate) return 'area'; 
     if (Array.isArray(first.y)) return 'rangeBar';
+    if (first?.z !== undefined) return 'bubble';
     return 'line';
   }
 
   // Label/Value pairs
   if (first?.label !== undefined && first?.value !== undefined) return 'donut';
 
-  return 'bar'; // Reliable default
+  return 'bar';
 };
 
 const VizlyChart = forwardRef<VizlyRef, VizlyProps>(
@@ -62,103 +65,86 @@ const VizlyChart = forwardRef<VizlyRef, VizlyProps>(
     // 1. Determine Type
     const finalType = useMemo(() => type || inferChartType(data), [data, type]);
 
-    // 2. Format Series
+    // 2. Safe Data Formatter (Prevents .slice errors)
     const formatSeries = useCallback((incomingData: any[], chartType: string) => {
+      const safeData = Array.isArray(incomingData) ? incomingData : [];
       const isCircular = ["pie", "donut", "radialBar"].includes(chartType);
-      if (isCircular) {
-        return incomingData.map(item => (typeof item === 'object' ? item.value : item));
-      }
-      return [{ name: "Series 1", data: incomingData }];
-    }, []);
 
-    // 3. Imperative API (Exposing internal Apex methods)
+      if (isCircular) {
+        return safeData.length > 0 
+          ? safeData.map(item => (typeof item === 'object' ? item.value : item))
+          : [];
+      }
+      return [{ name: options?.seriesName || "Series 1", data: safeData }];
+    }, [options?.seriesName]);
+
+    // 3. Imperative API (Exposing internal Apex methods safely)
     useImperativeHandle(ref, () => ({
       zoomIn: () => {
-        if (!chartInstance.current) return;
-      
-        // Cast to 'any' to bypass the missing 'w' property definition in TS
-        const internalChart = chartInstance.current as any;
-        const { min, max } = internalChart.w.globals.lastXAxis || { 
-          min: internalChart.w.globals.minX, 
-          max: internalChart.w.globals.maxX 
-        };
-      
+        const chart = chartInstance.current as any;
+        if (!chart?.w) return;
+        const { min, max } = chart.w.globals.lastXAxis || { min: chart.w.globals.minX, max: chart.w.globals.maxX };
         const diff = (max - min) * 0.2;
-        chartInstance.current.zoomX(min + diff, max - diff);
+        chart.zoomX(min + diff, max - diff);
       },
       zoomOut: () => chartInstance.current?.resetSeries(),
       filter: (newData: any[]) => {
         chartInstance.current?.updateSeries(formatSeries(newData, finalType));
       },
       reset: () => chartInstance.current?.resetSeries(),
-      download: (format: "png" | "csv" | "svg") => {
-        // Triggers the built-in Apex export
-        const chart = modalChartInstance.current || chartInstance.current;
-        const base64 = chart?.exports.exportToCSV(); // Example for CSV
-        // For standard formats, Apex's toolbar is more reliable
-      }
     }));
 
     // 4. Config Generator
-    const getBaseConfig = useCallback((chartHeight: number | string, isModal: boolean) => {
+    const getBaseConfig = useCallback((chartHeight: number | string) => {
       return {
         ...options,
         chart: {
+          toolbar: { show: true },
           ...options.chart,
           type: finalType,
           height: chartHeight,
-          toolbar: {
-            show: true, // This enables the built-in Download/Zoom/Pan buttons
-            tools: {
-              download: true, // This adds the 'hamburger' menu for PNG/SVG/CSV
-              selection: true,
-              zoom: true,
-              zoomin: true,
-              zoomout: true,
-              pan: true,
-              reset: true
-            },
-            autoSelected: 'zoom' 
-          },
-          animations: { enabled: true, easing: 'easeinout', speed: 800 }
         },
         series: formatSeries(data, finalType),
         labels: options.labels || (data[0]?.label ? data.map((d: any) => d.label) : undefined),
-        theme: { mode: 'light' }
       };
     }, [data, finalType, options, formatSeries]);
 
-    // 5. Initialize/Update Main Chart
+    // 5. Main Chart Lifecycle
     useEffect(() => {
       if (!chartRef.current) return;
 
-      const config = getBaseConfig(height, false);
+      const config = getBaseConfig(height);
 
       if (!chartInstance.current) {
-        chartInstance.current = new ApexCharts(chartRef.current, config);
-        chartInstance.current.render();
+        const newChart = new ApexCharts(chartRef.current, config);
+        chartInstance.current = newChart;
+        newChart.render().catch(() => {}); // Catch silent render errors
       } else {
-        chartInstance.current.updateOptions(config);
+        chartInstance.current.updateOptions(config, false, true);
       }
+
+      return () => {
+        if (chartInstance.current) {
+          chartInstance.current.destroy();
+          chartInstance.current = null; // Important: Clear ref on unmount
+        }
+      };
     }, [getBaseConfig, height]);
 
-    // 6. Initialize Modal Chart
+    // 6. Modal Chart Lifecycle
     useEffect(() => {
       if (showModal && modalChartRef.current) {
-        const config = getBaseConfig("100%", true);
+        const config = getBaseConfig("100%");
         modalChartInstance.current = new ApexCharts(modalChartRef.current, config);
-        modalChartInstance.current.render();
+        modalChartInstance.current.render().catch(() => {});
       }
       return () => {
-        modalChartInstance.current?.destroy();
-        modalChartInstance.current = null;
+        if (modalChartInstance.current) {
+          modalChartInstance.current.destroy();
+          modalChartInstance.current = null;
+        }
       };
     }, [showModal, getBaseConfig]);
-
-    // Cleanup
-    useEffect(() => {
-      return () => chartInstance.current?.destroy();
-    }, []);
 
     return (
       <div style={containerStyle}>
@@ -172,7 +158,7 @@ const VizlyChart = forwardRef<VizlyRef, VizlyProps>(
           <div style={modalOverlayStyle}>
             <div style={modalContentStyle}>
               <div style={modalHeaderStyle}>
-                <h3 style={{ margin: 0, fontSize: '1.2rem' }}>Detailed Visualization</h3>
+                <h3 style={{ margin: 0 }}>Detailed View</h3>
                 <button onClick={() => setShowModal(false)} style={closeButtonStyle}>
                   <RxCross1 size={22} />
                 </button>
@@ -188,71 +174,36 @@ const VizlyChart = forwardRef<VizlyRef, VizlyProps>(
 
 // --- Styles ---
 const containerStyle: React.CSSProperties = {
-  position: "relative",
-  width: "100%",
-  background: "#fff",
-  border: "1px solid #e2e8f0",
-  borderRadius: "12px",
-  padding: "15px",
-  boxShadow: "0 1px 3px rgba(0,0,0,0.1)"
+  position: "relative", width: "100%", background: "#fff",
+  border: "1px solid #e2e8f0", borderRadius: "12px", padding: "15px"
 };
 
 const iconButtonStyle: React.CSSProperties = {
-  position: "absolute",
-  right: "15px",
-  top: "15px",
-  zIndex: 10,
-  background: "#f8fafc",
-  border: "1px solid #cbd5e1",
-  borderRadius: "6px",
-  cursor: "pointer",
-  padding: "6px",
-  display: "flex",
-  alignItems: "center",
-  color: "#475569",
-  transition: "all 0.2s"
+  position: "absolute", right: "15px", top: "15px", zIndex: 10,
+  background: "#f8fafc", border: "1px solid #cbd5e1", borderRadius: "6px",
+  cursor: "pointer", padding: "6px", display: "flex", alignItems: "center"
 };
 
 const modalOverlayStyle: React.CSSProperties = {
-  position: "fixed",
-  top: 0, left: 0,
-  width: "100vw", height: "100vh",
-  backgroundColor: "rgba(15, 23, 42, 0.9)",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  zIndex: 9999,
-  backdropFilter: "blur(4px)"
+  position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh",
+  backgroundColor: "rgba(15, 23, 42, 0.9)", display: "flex",
+  justifyContent: "center", alignItems: "center", zIndex: 9999, backdropFilter: "blur(4px)"
 };
 
 const modalContentStyle: React.CSSProperties = {
-  width: "90%",
-  height: "85%",
-  backgroundColor: "#fff",
-  borderRadius: "16px",
-  padding: "25px",
-  display: "flex",
-  flexDirection: "column",
-  boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)"
+  width: "90%", height: "85%", backgroundColor: "#fff", borderRadius: "16px",
+  padding: "25px", display: "flex", flexDirection: "column"
 };
 
 const modalHeaderStyle: React.CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  marginBottom: "20px",
-  paddingBottom: "15px",
-  borderBottom: "1px solid #f1f5f9"
+  display: "flex", justifyContent: "space-between", alignItems: "center",
+  marginBottom: "20px", paddingBottom: "15px", borderBottom: "1px solid #f1f5f9"
 };
 
 const closeButtonStyle: React.CSSProperties = {
-  background: "#f1f5f9",
-  border: "none",
-  borderRadius: "50%",
-  width: "36px", height: "36px",
-  display: "flex", alignItems: "center", justifyContent: "center",
-  cursor: "pointer",
-  color: "#64748b"
+  background: "#f1f5f9", border: "none", borderRadius: "50%",
+  width: "36px", height: "36px", cursor: "pointer", display: "flex",
+  alignItems: "center", justifyContent: "center"
 };
 
 export default VizlyChart;
